ec19acf13c5171bff1c7cd55e4939f4cd9037d3f
Maxim Prav <mxmlknv@mail.ru>
2023-02-25 12:49:25 +0300

    Задача 38 с поиском разницы между макс и мин числами в массиве

---
+++ Task38/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+} 
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+    for (int i=0; i<size; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int MaxInArray(int[] array)
+{
+    int size = array.Length;
+    int max = array[0];
+    for(int i=1; i<size; i++)
+    {
+        if (array[i]>max)
+        {
+            max = array[i];
+        }
+    }
+    Console.WriteLine($"Максимальное число в массиве {max}");
+    return max;
+}
+
+int MinInArray(int[] array)
+{
+    int size = array.Length;
+    int min = array[0];
+    for(int i=1; i<size; i++)
+    {
+        if (array[i]<min)
+        {
+            min = array[i];
+        }
+    }
+    Console.WriteLine($"Минимальное число в массиве {min}");
+    return min;
+}
+
+int[] array88 = GetArray(6, 0, 20);
+PrintArray(array88);
+// MaxInArray(array88);
+// MinInArray(array88);
+int diff = MaxInArray(array88) - MinInArray(array88);
+Console.WriteLine($"Разница между максимальным и минимальным числом равна {diff}");
